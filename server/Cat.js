2442a709022ca8e9d2cff79339d1e08811e515f4
Bill Leonhardt <iamleonhardt@gmail.com>
2020-07-30 21:20:01 -0700

    Added grass background

--- server/Cat.js
+++ server/Cat.js
@@ -3,7 +3,7 @@ const Player = require("./Player");
 class Cat extends Player {
   constructor(...args) {
     super(...args);
-    this.color = "#C5851C";
+    this.color = "#eec39a";
     this.radius = 30;
   }
 }
